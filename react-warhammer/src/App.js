34b4642acb3c9268954e8b3500e74a99582e5e9d
gianluca-fernandez-QA <gianluca.fernandez@academytrainee.com>
2020-09-15 12:09:29 +0100

    Initiliased React Project and injected Bootstrap

---
+++ react-warhammer/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div class="h-100 row">
+      <div class="col-sm-5">
+        <h1>HELLO</h1>
+      </div>
+      <div class="col-sm-7">
+        <h1>GOODBYE</h1>
+      </div>
+    </div>
+  </>
+  );
+}
+
+export default App;
